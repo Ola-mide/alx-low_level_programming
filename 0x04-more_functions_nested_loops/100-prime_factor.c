0452da329dd378d60d7796ab73000fed88de61cd
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-07-12 08:01:01 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - looking for largest prime number of 
+ * main - looking for largest prime number of 612852475143
  *
  * Return: Always 0
  */
-int main(void
+int main(void)
+{
+	long int a = 1231952;
+	long int b, c, d, e;
+
+	for (b = 2; b < a; b++)
+	{
+		c = a % b;
+		if (c == 0)
+		{
+			for (d = 0; d <= b; d++)
+			{
+				if (((b % d) == 0) && ((b / d) == 1))
+				{
+					e = b;
+				}
+			}
+		}
+	}
+	printf("%ld", e);
+	printf("\n");
+	return (0);
+}
