33ba512e6eee0a1edaf2b49890753c15ed628798
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-07-31 05:57:56 -0700

    a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - using a function like macro that finds the sum of two numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
+}
