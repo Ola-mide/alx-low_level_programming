e88b80c57f6b60ea199704a71f6e3d755ecd4281
Abdulrahman Oyetade <abdulrahmanoyetade@gmail.com>
2022-08-01 06:14:25 -0700

    a function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - assigning values to struct members and printing them
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
+    return (0);
+}
